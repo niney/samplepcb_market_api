3c23b0196721f3aeb5b7fc66c46689b3ef611f87
Aosi <mania001@nate.com>
2021-05-10 15:10:46 +0900

    createAccount bug test

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { json } from 'express'
 import * as moment from 'moment'
 import { JwtService } from 'src/jwt/jwt.service'
 import { Repository } from 'typeorm'
@@ -64,8 +65,8 @@ export class UsersService {
         ok: true,
         user: newUser,
       }
-    } catch {
-      return { ok: false, error: "Couldn't create account" }
+    } catch (e) {
+      return { ok: false, error: JSON.stringify(e) }
     }
   }
 
